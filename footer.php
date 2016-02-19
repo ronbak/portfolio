964c96a476c8b0f591d06554d4e735a82515a466
joshuanijman <joshua@MacBook-Pro-van-Joshua.local>
2016-02-19 17:20:23 +0100

    cv and footer.php included

---
+++ footer.php
@@ -0,0 +1,44 @@
+            <section class="section contact" id="contact">
+                <div class="mainContainer">
+                    <h2>Lets grab some coffee</h2>
+                    <p>
+                        Interested to meet me?<br>It would be great if you could offer me an internship, but we can also have a friendly conversation.
+                        Whatever the cause, let's get in touch!
+                    </p>
+                    <div class="linkContain">
+                        <a href="mailto:joshua.nijman@gmail.com?SUBJECT=Website contact" class="link">E-mail</a>
+                        <div class="spacer"></div>
+                        <a href="https://www.linkedin.com/pub/joshua-nijman/57/566/b41" class="link">LinkedIn</a>
+                    </div>
+                </div>
+
+            </section>
+
+            <div id="ctc" class="section">
+                <div class="mainContainer">
+                        
+                    <h2>Give me a call</h2>
+                    <p> 
+                        +31 6 555 20 21 1
+                    </p>
+                       
+                </div>
+            </div>
+            
+        </div>  
+        </div>
+    </div>
+      
+      
+      
+      
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        
+    </body>
+</html>
